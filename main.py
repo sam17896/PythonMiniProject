64324155ff9e3506bf8cbc61dcec891e2a9b4f6a
Ahsan Sohail <k142109@nu.edu.pk>
2017-05-24 23:22:40 +0500

    Envrionment Setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+min = 1
+max = 6
+
+while(True):
+    value = random.randint(min, max)
+    print("die rolled " + str(value))
+    print("You want to roll again?")
+    print("prgram")
+    break
+
+
